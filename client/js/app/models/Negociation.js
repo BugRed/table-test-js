29488ef34bfe8d5eb2c380969a5d8168cba667a9
unknown <o.adrian.rodrigues@gmail.com>
2023-09-27 16:35:27 -0300

    privacit emulated in js for date

--- client/js/app/models/Negociation.js
+++ client/js/app/models/Negociation.js
@@ -5,7 +5,10 @@ class Negociation {
 
         //usar _ antes do nome da propriedade é um aviso para variaveis que deveriam ser só de leitura
         //pegando a data do momento
-        this._date = date || new Date();
+        //usar uma nova instancia de uma referencia para criar um novo objeto dentro da classe
+        //garante que se usada uma variavel para construir a classe
+        //não poderá mudar a data mudando a variavel
+        this._date = new Date(date.getTime()) || new Date();
         this._quantity = quantity || 1;
         this._value = value || 0.0;
 
@@ -17,8 +20,11 @@ class Negociation {
         return this._quantity * this._value;
     }
 
-    get data(){
-        return this._date;
+    get date(){
+        //date é um objeto portanto não pode ser congelado aqui,
+        //para impedir que alguem altere a data ao inves de enviar 
+        //a referencia da data mandamos uma variavel novo como referencia
+        return new Date(this._date.getTime());
     }
 
     get quantity(){
